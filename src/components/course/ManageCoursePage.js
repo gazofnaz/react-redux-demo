f90cac52f2739e971601c77d7a93d0e201b3b5eb
gazofnaz <gazofnaz31@hotmail.com>
2017-03-12 08:43:37 +0000

    add manage course form

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,29 +2,55 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
 
     constructor(props, context) {
         super(props, context);
+        // setup local state for the form
+        this.state = {
+            // create new object, assign all the properties from old to new
+            course: Object.assign({}, props.course),
+            errors: {}
+        };
     }
 
     // Don't define new functions inside a render call, it impacts performance
     render() {
-
         return (
-            <h1>Manage Course</h1>
+            <CourseForm
+                allAuthors={[]}
+                course={this.state.course}
+                errors={this.state.errors}
+            />
         );
     }
 }
 
+/**
+ * The props object in the constructor must have a course object
+ *
+ * @type {{course: *}}
+ */
 ManageCoursePage.propTypes = {
-    // myProp: PropTypes.string.isRequired
+    course: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
+
+    // not sure why we do this
+    let course = {
+        id: '',
+        watchHref: '',
+        title: '',
+        authorId: '',
+        length: '',
+        category: ''
+    };
+
     return {
-        state: state
+        course: course
     };
 }
 
